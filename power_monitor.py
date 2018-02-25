f9da9f85fd775553a61b1d3e53d19219e22c8c1f
aenea <scott@aenea.org>
2018-02-25 10:48:15 -0500

    fix typo

--- power_monitor.py
+++ power_monitor.py
@@ -25,7 +25,7 @@ class PowerMonitor(hass.Hass):
 
             # start an idle timer if one is not already running
             if self.idle_timer is not None:
-                self.idle_timer = self.run_int(self.entity_idle, self.max_idle_seconds)
+                self.idle_timer = self.run_in(self.entity_idle, self.max_idle_seconds)
         
         elif float(new) >= self.on_load and entity_state == 'Idle':
             # mark the entity as running
