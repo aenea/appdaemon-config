4652be0233738548b3b8323f208cf579a252e0e4
aenea <scott@aenea.org>
2018-02-25 12:58:05 -0500

    Notification

--- power_monitor.py
+++ power_monitor.py
@@ -1,13 +1,21 @@
 import appdaemon.plugins.hass.hassapi as hass
+from datetime import datetime
+import time
 
 class PowerMonitor(hass.Hass):
 
     def initialize(self):
 
         self.max_idle_seconds = self.args['max_idle_seconds']
+        self.notify = self.args['notify']
+        self.notify_message = self.args['notify_message']
+        self.notify_target = self.args['notify_target']
+        self.notify_title = self.args['notify_title']
         self.off_load = self.args['off_load']
         self.on_load = self.args['on_load']
         self.sensor_entity = self.args['sensor_entity']
+        self.start_time = None
+        self.stop_time = None
         self.tracking_entity = self.args['tracking_entity']
         self.idle_timer = None
 
@@ -27,7 +35,8 @@ class PowerMonitor(hass.Hass):
             # mark the entity as running
             self.select_option(self.tracking_entity, 'Running')
 
-            self.call_service('notify/pushbullet_scott', title='washing machine', message='done')
+            # track the start time
+            self.start_time = datetime.now()
 
             # cancel any running idle timers
             if self.idle_timer is not None:
@@ -43,6 +52,19 @@ class PowerMonitor(hass.Hass):
         if entity_state is not 'Idle':
             # mark the entity as idle
             self.select_option(self.tracking_entity, 'Idle')
+
+            # track the stop time
+            self.stop_time = datetime.now()
+
+            if self.notify is True:
+                tdelta = self.stop_time - self.start_time
+                elapsed_time = time.strftime('%H:S', tdelta)
+
+                self.notify_message += ("Elapsed time {}".format(elapsed_time))
+                
+                # send out the notification
+                self.call_service(self.notify_target, title=self.notify_title,
+                  message=self.notify_message)                
 
             self.log("{} is off.".format(self.sensor_entity), level='INFO')
 
