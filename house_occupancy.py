24d8e42209f77e9af9758f14c9383242be58fc7f
aenea <scott@aenea.org>
2018-02-27 20:49:05 -0500

    fixes

--- house_occupancy.py
+++ house_occupancy.py
@@ -10,53 +10,53 @@ class HouseOccupancy(hass.Hass):
         self.listen_state(self.house_unoccupied, self.sensor, new='off')
 
 
-# someone has arrived to an empty house
-def house_occupied(self, entity, attribute, old, new, kwargs):
+    # someone has arrived to an empty house
+    def house_occupied(self, entity, attribute, old, new, kwargs):
 
-    # stop the dog music
-    self.call_service('media_player/squeezebox_call_method', 
-      entity_id='media_player.office', command='power', 
-      parameters='0')
-    self.log("Dog music stopped", level='INFO')
+        # stop the dog music
+        self.call_service('media_player/squeezebox_call_method', 
+        entity_id='media_player.office', command='power', 
+        parameters='0')
+        self.log("Dog music stopped", level='INFO')
 
-    # clear the thermostat away mode
-    self.call_service('climate/set_away_mode', entity_id='climate.Home',
-      away_mode='false')
-    self.log("Thermostat away mode cleared", level='INFO')
+        # clear the thermostat away mode
+        self.call_service('climate/set_away_mode', entity_id='climate.Home',
+        away_mode='false')
+        self.log("Thermostat away mode cleared", level='INFO')
 
-    # get the house mode
-    house_mode = self.get_state('input_select.house_mode', attribute='state')
-    
-    if house_mode == 'Night':
-        # turn on the welcome lights
-        self.turn_on('group.welcome_lights')
-        self.log("Welcome lights turned on", level='INFO')
-    else:
-        self.log("Welcome lights declined, house mode is {}"
-          .format(house_mode))
+        # get the house mode
+        house_mode = self.get_state('input_select.house_mode', attribute='state')
+        
+        if house_mode == 'Night':
+            # turn on the welcome lights
+            self.turn_on('group.welcome_lights')
+            self.log("Welcome lights turned on", level='INFO')
+        else:
+            self.log("Welcome lights declined, house mode is {}"
+            .format(house_mode))
 
-# everone has left the house
-def house_unoccupied(self, entity, attribute, old, new, kwargs):
+    # everone has left the house
+    def house_unoccupied(self, entity, attribute, old, new, kwargs):
 
-    # turn off lights
-    self.turn_off('group.all_lights')
-    self.turn_off('group.all_switches')
-    self.log("All lights turned off", level='INFO')
+        # turn off lights
+        self.turn_off('group.all_lights')
+        self.turn_off('group.all_switches')
+        self.log("All lights turned off", level='INFO')
 
-    # turn off remotes
-    self.turn_off('group.remotes')
-    self.log('All remotes turned off', level='INFO')
+        # turn off remotes
+        self.turn_off('group.remotes')
+        self.log('All remotes turned off', level='INFO')
 
-    # set the thermostat away mode
-    self.call_service('climate/set_away_mode', entity_id='climate.Home',
-      away_mode='true')
-    self.log("Thermostat away mode cleared", level='INFO')
+        # set the thermostat away mode
+        self.call_service('climate/set_away_mode', entity_id='climate.Home',
+        away_mode='true')
+        self.log("Thermostat away mode cleared", level='INFO')
 
-    # start the dog music
-    self.call_service('media_player/squeezebox_call_method', 
-      entity_id='media_player.office', command='playlist', 
-      parameters='["loadtracks", "album.titlesearch=Through a dogs ears"]')
-    self.log("Dog music started", level='INFO')
+        # start the dog music
+        self.call_service('media_player/squeezebox_call_method', 
+        entity_id='media_player.office', command='playlist', 
+        parameters='["loadtracks", "album.titlesearch=Through a dogs ears"]')
+        self.log("Dog music started", level='INFO')
 
 
 
