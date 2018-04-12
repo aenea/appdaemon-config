5e34c79a721775dd74ba3297ead03c7d1f920930
aenea <scott@aenea.org>
2018-04-11 21:28:29 -0400

    refactor

--- house_occupancy.py
+++ house_occupancy.py
@@ -1,5 +1,6 @@
 import appdaemon.plugins.hass.hassapi as hass
 
+
 class HouseOccupancy(hass.Hass):
 
     def initialize(self):
@@ -8,113 +9,196 @@ class HouseOccupancy(hass.Hass):
 
         self.listen_state(self.home_occupied, self.sensor, new='on')
         self.listen_state(self.home_unoccupied, self.sensor, new='off')
-        self.listen_state(self.set_occupancy_on, 'group.presence_all', 
+        self.listen_state(self.set_occupancy_on, 'group.presence_all',
                           new='home', old='not_home')
-        self.listen_state(self.set_occupancy_off, 'group.presence_all', 
+        self.listen_state(self.set_occupancy_off, 'group.presence_all',
                           new='not_home', old='home')
         self.listen_state(self.someone_arrives, 'device_tracker', new='home')
-        self.listen_state(self.door_opens, 
-                          'binary_sensor.back_door_sensor_sensor', new='on')
+        self.listen_state(
+            self.door_opens,
+            'binary_sensor.back_door_sensor_sensor',
+            new='on'
+        )
         self.listen_state(
             self.climate_mode_change,
             'climate.home',
             attribute='climate_mode'
         )
-    
-    def door_opens(self, entity, attribute, old, new, kwargs):
-        """ Turn off the porch light when the back door opens if the porch 
-        light was turned on by automation
-        """
-        porch_light_status = self.get_state('input_select.front_porch_light_status',
-          attribute='state')
 
-        if porch_light_status == 'Automated':
+    def door_opens(self, entity, attribute, old, new, kwargs):
+
+        # Turn off the porch light when the back door opens if
+        # the porch light was turned on by automation
+        porch_light_status = self.get_state(
+            'input_select.front_porch_light_status',
+            attribute='state'
+        )
+
+        if porch_light_status.casefold() == 'automated':
             self.turn_off('switch.front_porch_light_switch_switch')
 
     def set_occupancy_on(self, entity, attribute, old, new, kwargs):
 
-        self.turn_on('input_boolean.home_occupancy')
-        self.log("Someone has arrived to an empty house", level='INFO')
+        self.turn_on(self.sensor)
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=('someone has arrived to an empty house')
+        )
 
-    def set_occupancy_off(self, entity, attribute, old, new, kwargs):        
-    
-        self.turn_off('input_boolean.home_occupancy')
-        self.log("Everyone has left", level='INFO')
+    def set_occupancy_off(self, entity, attribute, old, new, kwargs):
+
+        self.turn_off(self.sensor)
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=('everyone has left')
+        )
 
     def someone_arrives(self, entity, attribute, old, new, kwargs):
-        
+
         # get the house mode
-        house_mode = self.get_state('input_select.house_mode',
-          attribute='state')
-        
-        if house_mode == 'Night':
+        house_mode = self.get_state(
+            'input_select.house_mode',
+            attribute='state'
+        )
+
+        if house_mode.casefold() == 'night':
             # turn on the porch light
-            self.select_option('input_select.front_porch_light_status',
-                               'Automated')
-            self.turn_on('switch.porch_light_switch_switch')            
-            self.log('Porch light turned on for arrival', level='INFO')
-                    
-        self.log('{} has arrived. House mode is {}'.format(entity,
-          house_mode))
+            self.select_option(
+                'input_select.front_porch_light_status',
+                'Automated'
+            )
+            self.turn_on('switch.porch_light_switch_switch')
+            self.call_service(
+                'logbook/log',
+                entity_id=self.sensor,
+                domain='automation',
+                name='house_occupancy: ',
+                message=('porch light turned on for arrival')
+            )
 
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(
+                '{} has arrived. House mode is {}'.format(
+                    entity, house_mode
+                )
+            )
+        )
 
-    # someone has arrived to an empty house
     def home_occupied(self, entity, attribute, old, new, kwargs):
 
-        self.log("House is occupied", level='INFO')
-
         # stop the dog music
-        self.call_service('media_player/squeezebox_call_method', 
-          entity_id='media_player.office', 
-          command='power', 
-          parameters='0')
-        self.log("Dog music stopped", level='INFO')
-
-        # clear the thermostat away mode
-        self.call_service('climate/set_away_mode', 
-          entity_id='climate.Home',
-          away_mode='false')
-        self.log("Thermostat away mode cleared", level='INFO')
+        self.call_service(
+            'media_player/squeezebox_call_method',
+            entity_id='media_player.office',
+            command='power',
+            parameters='0'
+        )
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' dog music stopped')
+        )
 
         # get the house mode
-        house_mode = self.get_state('input_select.house_mode', attribute='state')
-        
-        if house_mode == 'Night':
+        house_mode = self.get_state(
+            'input_select.house_mode',
+            attribute='state'
+        )
+
+        if house_mode.casefold() == 'night':
             # turn on the welcome lights
             self.turn_on('group.welcome_lights')
-            self.log("Welcome lights turned on", level='INFO')
+            self.call_service(
+                'logbook/log',
+                entity_id=self.sensor,
+                domain='automation',
+                name='house_occupancy: ',
+                message=('welcome lights turned on')
+            )
         else:
-            self.log("Welcome lights declined, house mode is {}"
-            .format(house_mode))
+            self.call_service(
+                'logbook/log',
+                entity_id=self.sensor,
+                domain='automation',
+                name='house_occupancy: ',
+                message=(
+                    'welcome lights declined - house mode is {}'.format(
+                        house_mode
+                    )
+                )
+            )
 
-    # everyone has left the house
     def home_unoccupied(self, entity, attribute, old, new, kwargs):
 
-        self.log("House is not occupied", level='INFO')
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' house is not occupied')
+        )
 
         # turn off lights
         self.turn_off('group.lights')
         self.turn_off('group.switches')
-        self.log("All lights turned off", level='INFO')
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' all lights turned off')
+        )
 
         # turn off remotes
         self.turn_off('group.remotes')
-        self.log('All remotes turned off', level='INFO')
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' all remotes turned off')
+        )
 
-        # clear any hold modes on the thermostat
+        # resume thermostat schedule
         self.call_service(
             'climate/set_hold_mode',
             entity_id='climate.home',
             hold_mode='none'
         )
-        self.log("Thermostat away mode set", level='INFO')
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' thermostat schedule resumed')
+        )
 
         # start the dog music
-        self.call_service('media_player/squeezebox_call_method', 
-          entity_id='media_player.office', 
-          command='playlist', 
-          parameters=['loadtracks', 'album.titlesearch=Through a dogs ears'])
-        self.log("Dog music started", level='INFO')
+        self.call_service(
+            'media_player/squeezebox_call_method',
+            entity_id='media_player.office',
+            command='playlist',
+            parameters=['loadtracks', 'album.titlesearch=Through a dogs ears']
+        )
+        self.call_service(
+            'logbook/log',
+            entity_id=self.sensor,
+            domain='automation',
+            name='house_occupancy: ',
+            message=(' dog music started')
+        )
 
     def climate_mode_change(self, entity, attribute, old, new, kwargs):
 
@@ -124,14 +208,11 @@ class HouseOccupancy(hass.Hass):
             attribute='state'
         )
 
-        # if the thermostat changes to 'Away' and someone is home
-        # set a hold mode using the 'Home' climate
-        if new == 'Away' and house_occupancy == 'home':
+        # don't allow the thermostat to move to away mode if someone
+        # is home
+        if new.casefold() == 'away' and house_occupancy == 'home':
             self.call_service(
                 'climate/set_hold_mode',
                 entity_id='climate.home',
                 hold_mode='home'
-            )                    
-
-
-
+            )
