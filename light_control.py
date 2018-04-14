c20786283dbb74f323ab5e0f69fe437e40cb0fc8
aenea <scott@aenea.org>
2018-04-14 15:08:08 -0400

    wip

--- light_control.py
+++ light_control.py
@@ -71,5 +71,5 @@ class LightControl(hass.Hass):
                 self.call_service(
                     'homeassistant/turn_on',
                     entity_id=ct_light,
-                    kelvin=new
+                    kelvin=int(float(new))
                 )
