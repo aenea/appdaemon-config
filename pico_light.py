97c83624e9924476a5e51953445642ec10bd115b
aenea <scott@aenea.org>
2018-04-14 15:48:24 -0400

    wip

--- pico_light.py
+++ pico_light.py
@@ -16,6 +16,8 @@ class PicoLight(hass.Hass):
         
         self.listen_state(self.switch_on, self.actuator, new='1')
         self.listen_state(self.switch_off, self.actuator, new='4')
+        self.listen_state(self.brighter, self.actuator, new='8')
+        self.listen_state(self.dimmer, self.actuator, new='16')
 
     def switch_on(self, entity, attribute, old, new, kwargs):
 
@@ -28,4 +30,44 @@ class PicoLight(hass.Hass):
         # turn off the light
         self.log("{} off".format(new))
         self.turn_off(self.light_group)
+
+    def brighter(self, entity, attribute, old, new, kwargs):
+
+        # get the current brightness
+        current_brightness = int(float(self.get_state(
+            self.light_group,
+            attribute='brightness_pct'
+        )))
+
+        # round the current brightness to the nearest 10
+        current_brightness = round(current_brightness / 10, 0) * 10
+
+        # increase the brightness up by 10%
+        current_brightness += 10
+        
+        if current_brightness > 100:
+            current_brightness = 100
+
+        # change the light brightness
+        self.turn_on(self.light_group, brightness_pct=current_brightness)
+
+    def dimmer(self, entity, attribute, old, new, kwargs):
+
+        # get the current brightness
+        current_brightness = int(float(self.get_state(
+            self.light_group,
+            attribute='brightness_pct'
+        )))
+
+        # round the current brightness to the nearest 10
+        current_brightness = round(current_brightness / 10, 0) * 10
+
+        # decrease the brightness up by 10%
+        current_brightness -= 10
+        
+        if current_brightness < 5:
+            current_brightness = 5
+
+        # change the light brightness
+        self.turn_on(self.light_group, brightness_pct=current_brightness)
 
