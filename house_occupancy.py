e496bc170ce4711a9a89eeba5a2d61f345d7f0f6
aenea <scott@aenea.org>
2018-07-01 21:35:21 -0400

    wip

--- house_occupancy.py
+++ house_occupancy.py
@@ -28,13 +28,13 @@ class HouseOccupancy(hass.Hass):
             new='not_home',
             old='home'
         )
-        # track the arrival of individuals
-        self.listen_state(
-            self.someone_arrives,
-            'device_tracker',
-            new='home',
-            old='not_home'
-        )
+#        # track the arrival of individuals
+#        self.listen_state(
+#            self.someone_arrives,
+#            'device_tracker',
+#            new='home',
+#            old='not_home'
+#        )
 
         # track entry door status
         self.listen_state(self.door_opens, self.doors, new='on')
