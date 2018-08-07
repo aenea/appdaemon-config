7fc557587b1c38c23ab7d0a94c77aa60a8d19a77
aenea <scott@aenea.org>
2018-08-07 13:57:56 -0400

    wip

--- color_temperature.py
+++ color_temperature.py
@@ -14,7 +14,7 @@ class ColorTemperature(hass.Hass):
 
         # run the color temperature calculation once
         self.calc_temp(is_daylight=1)
-        
+
         # run the color temperature calculation every 5 minutes
         self.register_constraint("is_daylight")
         self.periodic = self.run_every(
