edc8ba8b03008a1ddc25868fea7d7c3e71b3301f
Scott Anderson <scott@aenea.org>
2018-09-07 16:08:05 -0400

    wip

--- sensor_light.py
+++ sensor_light.py
@@ -21,8 +21,8 @@ class SensorLight(hass.Hass):
 
         self.listen_state(self.sensor_on, self.sensor, new='on')
         self.listen_state(self.sensor_off, self.sensor, new='off')
-#        if self.stage_entity is not None:
-#            self.listen_state(self.stage_sensor_on, self.stage_entity, new='on')
+        if self.stage_entity is not None:
+            self.listen_state(self.stage_sensor_on, self.stage_entity, new='on')
         self.listen_state(self.actuator_off, self.actuator, new='off')
 
         self.listen_state(
@@ -35,11 +35,11 @@ class SensorLight(hass.Hass):
             self.tracker,
             new='on'
         )
-#        self.listen_state(
-#            self.tracker_stage,
-#            self.tracker,
-#            new='stage'
-#        )
+        self.listen_state(
+            self.tracker_stage,
+            self.tracker,
+            new='stage'
+        )
 
     @property
     def allowed_mode(self):
@@ -187,7 +187,7 @@ class SensorLight(hass.Hass):
             )
             return
 
-        if self.tracker_value != 'off' or self.tracker != 'stage':
+        if self.tracker_value != 'off' or self.tracker_value != 'stage':
             self.log('stage declined for lighting mode ' + self.current_state)
             return
 
